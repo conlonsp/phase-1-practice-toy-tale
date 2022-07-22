4ac30966c8e905ac40a7cea4c1adc25b896d952c
samuelconlon <conlonsp@yahoo.com>
2022-07-22 11:46:43 -0400

    commit tree 722b0dfe5647c3b94748c08dcdb4c550a2805097

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 let addToy = false;
+const form = document.querySelector('.add-toy-form')
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+  form.addEventListener('submit', addToys)
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +15,71 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetchToys()
 });
+
+function createCard(toy) {
+  const toyDiv = document.getElementById('toy-collection')
+  const div = document.createElement('div')
+  div.classList.add('card')
+  const h2 = document.createElement('h2')
+    h2.textContent = toy.name
+    div.appendChild(h2)
+  const img = document.createElement('img')
+    img.classList.add('toy-avatar')
+    img.src = toy.image
+    div.appendChild(img)
+  const p = document.createElement('p')
+    p.textContent = `${toy.likes} likes`
+    div.appendChild(p)
+  let btn = document.createElement('button')
+    btn.classList.add('like-btn')
+    btn.id = toy.id
+    btn.textContent = 'Like ❤️'
+    btn.addEventListener('click', (event) => {
+      event.preventDefault()
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          likes: toy.likes++
+        })
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        p.innerText = `${toy.likes} likes`
+      })
+    })
+    div.appendChild(btn)
+  toyDiv.append(div)
+}
+
+
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(data => data.forEach(toy => createCard(toy)))
+}
+
+function addToys(event) {
+  event.preventDefault()
+  const [name, image] = event.target
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name.value,
+      image: image.value,
+      likes: 0
+    })
+  })
+  .then(resp => resp.json())
+  .then(data => createCard(data))
+  form.reset()
+}
